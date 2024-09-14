7c518aa12c5911e8b25aee2616e027c169967ef7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-13 22:41:43 -0700

    rotates 2D list by 90 deg

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """rotates a 2D matrix by 90 degree
+    Args:
+        matrix (_type_): nested 2D list
+    """
+    length = len(matrix)
+    for i in range(length):
+        for j in range(i + 1, length):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(length):
+        matrix[i].reverse()
